dc8f047d689dd53a6db94eb386a158329b132da7
devashishkd <devashishkumar693@example.com>
2025-08-22 05:24:07 +0530

    deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig(({ mode }) => ({
   ],
   define: {
     'import.meta.env.MODE': JSON.stringify(mode),
-    'import.meta.env.VITE_API_URL': JSON.stringify("http://localhost:5000/api"),
-    'import.meta.env.VITE_SOCKET_URL': JSON.stringify("http://localhost:5000"),
+    'import.meta.env.VITE_API_URL': JSON.stringify("https://typex-jygr.onrender.com/api"),
+    'import.meta.env.VITE_SOCKET_URL': JSON.stringify("https://typex-jygr.onrender.com"),
     'import.meta.env.VITE_NODE_ENV': JSON.stringify(process.env.VITE_NODE_ENV || 'production')
   }
 }))
