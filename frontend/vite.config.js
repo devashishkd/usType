5a50ccc0e3c8319e8d2f049b2b9b6a3ed6059403
Devashish Kumar <151779268+devashishkd@users.noreply.github.com>
2025-08-22 00:08:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig(({ mode }) => ({
   ],
   define: {
     'import.meta.env.MODE': JSON.stringify(mode),
-    'import.meta.env.VITE_API_URL': JSON.stringify("http://localhost:5000/api"),
-    'import.meta.env.VITE_SOCKET_URL': JSON.stringify("http://localhost:5000"),
+    'import.meta.env.VITE_API_URL': JSON.stringify("https://wetype-ayft.onrender.com//api"),
+    'import.meta.env.VITE_SOCKET_URL': JSON.stringify("https://wetype-ayft.onrender.com/"),
     'import.meta.env.VITE_NODE_ENV': JSON.stringify(process.env.VITE_NODE_ENV || 'production')
   }
 }))
