dadfee7023e8c3036dde85b9f0c702d21b22db55
Devashish Kumar <151779268+devashishkd@users.noreply.github.com>
2025-08-22 00:14:39 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
